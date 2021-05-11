9d76f5251ee604e1dd14eb25de4f286bca3589fe
Nikki <user@Nikkis-Macbook.fios-router.home>
2021-05-10 22:00:55 -0400

    Nikki

--- client/src/Components/Search/Search.jsx
+++ client/src/Components/Search/Search.jsx
@@ -4,6 +4,14 @@ function Search() {
   return (
     <div>
       <h1>Search For Memoirs</h1>
+      <input
+                type='text'
+                name='search'
+                id='search'
+                autocomplete="off"
+                placeholder='Search user here...'
+
+            />
     </div>
   )
 }
